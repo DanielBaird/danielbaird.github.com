fb2e10428b12df9db889da750cc998ce417684c4
Daniel Baird <daniel@danielbaird.com>
2013-05-26 22:23:43 +1000

    added topic tags

--- assets/js/mousedots.js
+++ assets/js/mousedots.js
@@ -1,8 +1,9 @@
 
 dots = {};
 
-inHeader = false;
-nextTick = false;
+in_header = false;
+window.tick = false;
+tick_length = 100;
 
 handleDots = function() {
     dotthere = false;
@@ -10,54 +11,52 @@ handleDots = function() {
     w3_dots = [];
     moz_dots = [];
     offset = $header.offset();
-    $.each(dots, function(d) {
-        // step = Math.max(Math.round(dots[d].size / 10.0) || 1, 10);
-        step = 2;
+    $.each(dots, function(ref, d) {
+        step = 4;
         // is the mouse near this one?
-        xdiff = window.mx - offset.left - dots[d].x;
-        ydiff = window.my - offset.top - dots[d].y;
-        if (inHeader && xdiff < 15 && xdiff > -15 && ydiff < 15 && ydiff > -15) {
+        xdiff = window.mx - offset.left - d.x;
+        ydiff = window.my - offset.top - d.y;
+        if (dotthere = (in_header && xdiff < 15 && xdiff > -15 && ydiff < 15 && ydiff > -15)) {
             // increment this one..
-            dots[d].size += step;
-            dots[d].age = Math.max(0, dots[d].age - step/100);
-            dotthere = true;
+            d.size += step;
+            d.age = Math.max(-0.33, d.age - step/100);
         } else {
-            dots[d].size += step;
-            dots[d].age += step/200;
+            d.size += step;
+            d.age += step/200;
         }
-        // if (dots[d].size < 1) {
-        if (dots[d].age > 1) {
-            delete dots[d];
+        if (d.age > 1) {
+            delete dots[ref];
         } else {
-            x = dots[d].x;
-            y = dots[d].y;
-            size = dots[d].size;
-            opac = 1 - dots[d].age;
-            moz_dots.push('-moz-radial-gradient('+x+'px '+y+'px, circle, rgba(255,0,0, '+ opac +') 0%, rgba(255,255,255, 0) '+ size +'px)');
-            w3_dots.push('radial-gradient('+ size +'px at '+x+'px '+y+'px, rgba(255,0,0, '+ opac +'), rgba(255,255,255, 0))');
+            x = d.x;
+            y = d.y;
+            size = d.size;
+            opac = 0.66 - d.age;
+            moz_dots.push('-moz-radial-gradient('+x+'px '+y+'px, circle, rgba(255,0,0, '+ opac +'), rgba(255,0,0, 0) '+ size +'px)');
+            w3_dots.push('radial-gradient('+ size +'px at '+x+'px '+y+'px, rgba(255,0,0, '+ opac +'), rgba(255,0,0, 0))');
         }
     });
-    if (!dotthere && inHeader) {
+    if (!dotthere && in_header) {
         ref = (window.mx - offset.left) + ',' + (window.my - offset.top);
         dots[ref] = { x: (window.mx - offset.left), y: (window.my - offset.top), size: 7, age: 0 };
     }
-    $header.css('background', moz_dots.join(','));
-    $header.css('background', w3_dots.join(','));
+//    $header.css('background', moz_dots.join(','));
+    $header.css('background-image', w3_dots.join(','));
 
-    setTimeout("handleDots()", 100);
+    window.tick = setTimeout("handleDots()", tick_length);
 }
 
 $( function() {
-    setTimeout("handleDots()", 50);
+    window.tick = setTimeout("handleDots()", tick_length);
+//    setInterval("console.log(Object.keys(window.dots).length)", 1000);
     $header = $('.page > header')
     $header.on('mousemove', function(e) {
         window.mx = e.pageX
         window.my = e.pageY
     });
     $header.mouseenter( function(e) {
-        inHeader = true;
+        in_header = true;
     });
     $header.mouseleave( function(e) {
-        inHeader = false;
+        in_header = false;
     });
 });
